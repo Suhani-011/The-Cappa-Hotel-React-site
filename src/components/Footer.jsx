c2279602c387dc95caa6212a6ec3d069338dc175
Suhani-011 <suhaanijain.11@gmail.com>
2025-11-22 16:21:23 +0530

    solved a image icon issue

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,5 @@
+import callIcon from "../assets/images/call.png";
+
 const Footer = () => {
   return (
     <div>
@@ -5,19 +7,25 @@ const Footer = () => {
         <div className="footer-top position-relative">
           <div className="container">
             <div className="row">
+
+              {/* About */}
               <div className="col-md-4">
                 <div className="footer-column mb-30 footer-about">
                   <h3 className="footer-title">About Hotel</h3>
-                  <p className="footer-about-text">Welcome to the best five-star deluxe hotel in New York. Hotel elementum sesue the aucan vestibulum aliquam justo in sapien rutrum volutpat.</p>
-                  <div className="footer-language"> 
+                  <p className="footer-about-text">
+                    Welcome to the best five-star deluxe hotel in New York. Hotel elementum sesue the aucan vestibulum aliquam justo in sapien rutrum volutpat.
+                  </p>
+                  <div className="footer-language">
                     <i className="ri-global-line" />
-                    <select onchange="location = this.value;">
+                    <select>
                       <option>English</option>
                       <option>German</option>
                     </select>
                   </div>
                 </div>
               </div>
+
+              {/* Explore */}
               <div className="col-md-3 offset-md-1">
                 <div className="footer-column footer-explore clearfix">
                   <h3 className="footer-title">Explore</h3>
@@ -27,22 +35,27 @@ const Footer = () => {
                     <li className="pt-2"><a>Restaurant</a></li>
                     <li className="pt-2"><a>Spa &amp; Wellness</a></li>
                     <li className="pt-2"><a>About Hotel</a></li>
-                    <li className="pt-2"><a> Contact</a></li>
+                    <li className="pt-2"><a>Contact</a></li>
                   </ul>
                 </div>
               </div>
+
+              {/* Contact */}
               <div className="col-md-4">
                 <div className="footer-column footer-contact mb-30 position-relative">
                   <h3 className="footer-title">Contact</h3>
-                  <p className="footer-contact-text">1616 Broadway NY, New York 10001
-                    <br />United States of America
+                  <p className="footer-contact-text">
+                    1616 Broadway NY, New York 10001 <br />
+                    United States of America
                   </p>
+
                   <div className="footer-contact-info">
                     <p className="footer-contact-phone m-0">
-                      <img src="./src/assets/images/call.png" alt="call" /> 855 100 4444
+                      <img src={callIcon} alt="call" /> 855 100 4444
                     </p>
                     <p className="footer-contact-mail m-0">info@mountainhotel.com</p>
                   </div>
+
                   <div className="footer-about-social-list d-flex align-items-center">
                     <a href="#"><i className="ri-instagram-line" /></a>
                     <a href="#"><i className="ri-twitter-x-line" /></a>
@@ -50,25 +63,32 @@ const Footer = () => {
                     <a href="#"><i className="ri-facebook-circle-line" /></a>
                     <a href="#"><i className="ri-tiktok-line" /></a>
                   </div>
+
                 </div>
               </div>
+
             </div>
           </div>
         </div>
+
+        {/* Footer bottom */}
         <div className="footer-bottom">
           <div className="container">
             <div className="row">
               <div className="col-md-12">
                 <div className="footer-bottom-inner position-relative d-block">
-                  <p className="footer-bottom-copy-right m-0">© Copyright 2025 by <a href="#">DuruThemes.com</a></p>
+                  <p className="footer-bottom-copy-right m-0">
+                    © Copyright 2025 by <a href="#">DuruThemes.com</a>
+                  </p>
                 </div>
               </div>
             </div>
           </div>
         </div>
+
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
